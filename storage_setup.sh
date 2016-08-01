13c25ca7045b1342b606782deea7c7deb42edbcc
hanxt <hanxiaotongtong@163.com>
2016-08-01 15:32:30 +0800

    nginx module

--- storage_setup.sh
+++ storage_setup.sh
@@ -19,3 +19,33 @@ grep http.server_port /etc/fdfs/* |grep -v sample;
 grep store_path0 /etc/fdfs/* | grep -v sample;
 grep http.server_port /etc/fdfs/* | grep -v sample;
 
+
+#nginx need
+sudo apt-get  install -y build-essential libtool libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev;
+
+#nginx and fastdfs module setup
+cd $ROOTPATH/nginx-1.10.1
+./configure \
+  --prefix=/usr \
+  --sbin-path=/usr/sbin/nginx \
+  --conf-path=/etc/nginx/nginx.conf \
+  --error-log-path=/var/log/nginx/error.log \
+  --http-log-path=/var/log/nginx/access.log \
+  --pid-path=/var/run/nginx/nginx.pid  \
+  --lock-path=/var/lock/nginx.lock \
+  --user=root \
+  --group=root \
+  --with-http_ssl_module \
+  --with-http_flv_module \
+  --with-http_stub_status_module \
+  --with-http_gzip_static_module \
+  --with-pcre \
+  --add-module=${ROOTPATH}/fastdfs-nginx-module-master/src && make  && sudo make install
+
+
+sudo cp $ROOTPATH/fastdfs-nginx-module-master/src/mod_fastdfs.conf /etc/fdfs/ && \
+sudo sed -i "s:base_path=.*:base_path=${DATA_DIR}:g" /etc/fdfs/mod_fastdfs.conf && \
+sudo sed -i "s:store_path0=.*:store_path0=${DATA_DIR}:g" /etc/fdfs/mod_fastdfs.conf && \
+sudo sed -i "s/tracker_server=.*/tracker_server=${TRACKER_SERVER}:22122/g" /etc/fdfs/mod_fastdfs.conf && \
+sudo sed -i "s:log_filename=.*:log_filename=${DATA_DIR}/logs/mod_fastdfs.log:g" /etc/fdfs/mod_fastdfs.conf && \
+sudo sed -i "s:url_have_group_name.*:url_have_group_name=true:g" /etc/fdfs/mod_fastdfs.conf 
